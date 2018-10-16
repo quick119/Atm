989a604c5c6f5b40bc8df3810aeeac4833720fec
Simon Wang <quick119@hotmail.com>
2018-10-16 16:43:40 +0800

    單元25

--- app/src/main/java/com/quick/atm/MainActivity.java
+++ app/src/main/java/com/quick/atm/MainActivity.java
@@ -2,21 +2,33 @@ package com.quick.atm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOGIN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
     boolean logon = false;
-    String[] functions = null;
+    private List<Function> functions;
+    //    String[] functions = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +50,82 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //Recycler
+        setupFunctions();
+
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         //Adapter
-        FunctionAdapter adapter = new FunctionAdapter(this);
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        IconAdapter adapter = new IconAdapter();
         recyclerView.setAdapter(adapter);
+    }
+
+    private void setupFunctions() {
+        functions = new ArrayList<>();
+        String[] funcs = getResources().getStringArray(R.array.functions);
+        functions.add(new Function(funcs[0], R.drawable.func_transaction));
+        functions.add(new Function(funcs[1], R.drawable.func_balance));
+        functions.add(new Function(funcs[2], R.drawable.func_finance));
+        functions.add(new Function(funcs[3], R.drawable.func_contacts));
+        functions.add(new Function(funcs[4], R.drawable.func_exit));
+    }
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconHolder> {
+
+        @NonNull
+        @Override
+        public IconHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.item_icon, parent, false);
+            return new IconHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconHolder holder, int position) {
+            final Function function = functions.get(position);
+            holder.nameText.setText(function.getName());
+            holder.iconImage.setImageResource(function.getIcon());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClicked(function);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconHolder extends RecyclerView.ViewHolder {
+            ImageView iconImage;
+            TextView nameText;
+            public IconHolder(View itemView) {
+                super(itemView);
+                iconImage = itemView.findViewById(R.id.item_icon);
+                nameText = itemView.findViewById(R.id.item_name);
+
+            }
+        }
+    }
+
+    private void itemClicked(Function function) {
+        Log.d(TAG, "itemClicked:" + function.getName());
+        switch (function.getIcon()){
+            case R.drawable.func_transaction:
+                break;
+            case R.drawable.func_balance:
+                break;
+            case R.drawable.func_finance:
+                break;
+            case R.drawable.func_contacts:
+                break;
+            case R.drawable.func_exit:
+                finish();
+                break;
+        }
     }
 
     @Override
