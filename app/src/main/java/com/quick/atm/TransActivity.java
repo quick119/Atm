6b7632d84d4b2f0d72da8de3c38361f3403fde3b
Simon Wang <quick119@hotmail.com>
2018-10-19 21:09:32 +0800

    單元34

--- app/src/main/java/com/quick/atm/TransActivity.java
+++ app/src/main/java/com/quick/atm/TransActivity.java
@@ -1,9 +1,19 @@
 package com.quick.atm;
 
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +21,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,14 +33,20 @@ import okhttp3.Response;
 public class TransActivity extends AppCompatActivity {
 
     private static final String TAG = TransActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<Transaction> transactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans);
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 //        new TransTask().execute("http://atm201605.appspot.com/h");
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
+        final Request request = new Request.Builder()
                 .url("http://atm201605.appspot.com/h")
                 .build();
         client.newCall(request).enqueue(new Callback() {
@@ -39,9 +57,67 @@ public class TransActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG, "onResponse:" + response.body().string());
+                String json = response.body().string();
+                Log.d(TAG, "onResponse:" + json);
+                parseJSON(json);
             }
         });
+    }
+
+    private void parseJSON(String json) {
+        transactions = new ArrayList<>();
+        try {
+            JSONArray array = new JSONArray(json);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                transactions.add(new Transaction(object));
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        TransAdapter adapter = new TransAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public class TransAdapter extends RecyclerView.Adapter<TransAdapter.TransHolder> {
+
+        @NonNull
+        @Override
+        public TransHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.item_transaction, parent, false);
+            return new TransHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TransHolder holder, int position) {
+            Transaction tran = transactions.get(position);
+            holder.bindTo(tran);
+        }
+
+        @Override
+        public int getItemCount() {
+            return transactions.size();
+        }
+
+        public class TransHolder extends RecyclerView.ViewHolder {
+            TextView dateText;
+            TextView amountText;
+            TextView typeText;
+            public TransHolder(View itemView) {
+                super(itemView);
+                dateText = itemView.findViewById(R.id.item_date);
+                amountText = itemView.findViewById(R.id.item_amount);
+                typeText = itemView.findViewById(R.id.item_type);
+            }
+
+            public void bindTo(Transaction tran) {
+                dateText.setText(tran.getDate());
+                amountText.setText(tran.getAmount());
+                typeText.setText(tran.getType());
+            }
+        }
     }
 
     public class TransTask extends AsyncTask<String, Void, String> {
